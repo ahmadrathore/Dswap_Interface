2769b3785f7967a92c42dfcda86b3fa2cbafa608
infonet <ahmadrathore@gmail.com>
2021-09-09 15:32:23 +0900

    Remove history, due to sensitive data

---
+++ src/hooks/useIsTickAtLimit.ts
@@ -0,0 +1,24 @@
+// import { FeeAmount, nearestUsableTick, TickMath, TICK_SPACINGS } from '@uniswap/v3-sdk'
+import { FeeAmount, nearestUsableTick, TickMath, TICK_SPACINGS } from 'delioswap_v3_sdk'
+import { useMemo } from 'react'
+import { Bound } from 'state/mint/v3/actions'
+
+export default function useIsTickAtLimit(
+  feeAmount: FeeAmount | undefined,
+  tickLower: number | undefined,
+  tickUpper: number | undefined
+) {
+  return useMemo(
+    () => ({
+      [Bound.LOWER]:
+        feeAmount && tickLower
+          ? tickLower === nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount as FeeAmount])
+          : undefined,
+      [Bound.UPPER]:
+        feeAmount && tickUpper
+          ? tickUpper === nearestUsableTick(TickMath.MAX_TICK, TICK_SPACINGS[feeAmount as FeeAmount])
+          : undefined,
+    }),
+    [feeAmount, tickLower, tickUpper]
+  )
+}
